2d9693633ad0d87bcc436fae71fc091ebb446a8c
Andy Ngo <andyngo2021@gmail.com>
2022-07-28 13:02:55 -0700

    moved helper function

--- src/pages/Visualize.js
+++ src/pages/Visualize.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import Chart from "../components/Chart";
 import SongStack from "../components/SongStack";
 
+import {constructHistogram} from '../util/Functions';
+
 function Visualize()
 {
     const histogramCategories = ['danceability', 'energy', 'valence'];
@@ -26,49 +28,6 @@ function Visualize()
             Authorization: "Bearer " + localStorage.getItem("accessToken")
         }
     });
-
-
-    const constructHistogram = (data) => {
-        let formattedData = new Array(10).fill(0);
-        for (const val of data)
-        {
-            switch (true)
-            {
-                case val<=0.1:
-                    formattedData[0]++;
-                    break;
-                case val<=0.2:
-                    formattedData[1]++;
-                    break;
-                case val<=0.3:
-                    formattedData[2]++;
-                    break;
-                case val<=0.4:
-                    formattedData[3]++;
-                    break;
-                case val<=0.5:
-                    formattedData[4]++;
-                    break;
-                case val<=0.6:
-                    formattedData[5]++;
-                    break;
-                case val<=0.7:
-                    formattedData[6]++;
-                    break;
-                case val<=0.8:
-                    formattedData[7]++;
-                    break;
-                case val<=0.9:
-                    formattedData[8]++;
-                    break;
-                case val<=1.0:
-                    formattedData[9]++;
-                    break;
-            }
-        }
-
-        return formattedData;
-    }
 
 
     useEffect(() => {
