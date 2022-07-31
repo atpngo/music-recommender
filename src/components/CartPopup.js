9190709457b77b62103c25b1a988d67b291ac027
Andy Ngo <andytpngo@g.ucla.edu>
2022-07-30 23:20:10 -0700

    successfully fetched spotify playlists and put in autocomplete input

--- src/components/CartPopup.js
+++ src/components/CartPopup.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {Dialog, Stack, Paper, Autocomplete, TextField, Button} from '@mui/material';
 import AlbumCover from "../components/AlbumCover";
 import Slide from '@mui/material/Slide';
 import {styled} from '@mui/material/styles';
+import axios from "axios";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -12,37 +13,103 @@ const CustomTextField = styled(TextField)({
     'fieldset' : {
         borderRadius: '20px',
     },
-      '& .MuiOutlinedInput-root': {
+    '& .MuiOutlinedInput-root': {
         '& fieldset': {
-          borderColor: '#EF429F',
+            borderColor: '#EF429F',
         },
         '&:hover fieldset': {
-          borderColor: 'pink',
+            borderColor: 'pink',
         },
         '&.Mui-focused fieldset': {
-          borderColor: '#EF429F',
+            borderColor: '#EF429F',
         },
-      },
+    },
+
 });
+
+
+const CustomButton = styled(Button)(({theme}) => ({
+    color: 'rgb(255, 255, 255, 0.90)',
+    fontFamily: 'Montserrat, sans-serif',
+    textTransform: 'none',
+    backgroundColor: 'rgb(255, 255, 255, 0.25)',
+    borderRadius: '20px',
+    '&:hover': {
+        backgroundColor: 'rgb(255, 255, 255, 0.15)'
+    }
+}))
 
 function CartPopup()
 {
+    const [loading, setLoading] = useState(true);
+    const [playlists, setPlaylists] = useState([]);
+    const [selection, setSelection] = useState(null);
 
-    const [val, setVal] = useState('');
-    const tmp = [
-        'Cow1',
-        'Cow2',
-        'Cow3',
-        'Cow4',
-        'Cow5',
-        'Cow6',
-    ]
+    const color = '#FFFFFF';
+
+    const spotify = axios.create({
+        baseURL: 'https://api.spotify.com/v1/',
+        timeout: 1000,
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken")
+        }
+    });
+
+    useEffect(() => {
+        // calculate how many playlists they got
+        setLoading(true);
+        spotify.get('me/playlists')
+        .then((res) => {
+            let numPlaylists = res.data.total;
+            let numQueries = Math.ceil(numPlaylists/50);
+            if (numQueries > 0)
+            {
+                let pages = Array(numQueries).join(".").split(".");
+                pages = pages.map((item, index) => "me/playlists?limit=50&offset=" + (50*index).toString());
+                console.log(pages);
+                axios.all(pages.map(page => spotify.get(page)))
+                .then( res => {
+                    console.log(res);
+
+                    let playlistIds = [];
+                    for (const response of res)
+                    {
+                        playlistIds.push.apply(playlistIds, response.data.items);
+                    }
+                    // we only want the ones where we can actually edit
+                    const filteredPlaylist = playlistIds.filter(playlist => playlist.owner.id == localStorage.getItem("userID"));
+                    console.log(filteredPlaylist);
+                    setPlaylists(filteredPlaylist);
+                    setLoading(false);
+                })
+            }
+
+        });
+    }, []);
+
+    // this runs when we click the confirm button
+    const processQuery = (e) =>
+    {
+        // use playlist id and make an axios call to add songs in cart 
+        // clean up and reset cart
+    }
+
+    const handleInputChange = (e, newVal) => 
+    {
+        setSelection(newVal);
+        console.log(newVal);
+    }
+
+    if (loading)
+    {
+        return <div></div>
+    }
 
     return(
        <div>
         <Dialog maxWidth='xl' open={true} TransitionComponent={Transition} keepMounted PaperProps={{style: {borderRadius: 20, backgroundColor: '#E28BBA'}}}>
-                <div style={{width: '75vw', height: '75vh', display: 'flex', padding: '30px'}}>
-                    <Stack direction="column" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <div style={{width: '75vw', height: '75vh', display: 'flex', padding: '30px', justifyContent: "center"}}>
+                    <Stack direction="column" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}}>
                         {/* contains songs */}
                         <Paper elevation={10} sx={{backgroundColor: 'rgb(255,255,255,0.25)', height: '90%', width: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', overflow: 'hidden', alignContent: 'flex-start', padding: '30px', paddingLeft: '0px', paddingRight: '0px', borderRadius: '20px'}}>
                         <Paper elevation={0} sx={{backgroundColor: 'rgb(255,255,255,0)', height: '90%', width: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', overflow: 'auto', alignContent: 'flex-start', justifyContent: 'center', padding: '30px', borderRadius: '20px'}}>
@@ -58,9 +125,14 @@ function CartPopup()
                         </Paper>
                         </Paper>
                         {/* contains options */}
-                        <Stack direction="row" sx={{marginTop: '30px'}}>
-                            <Autocomplete disablePortal options={tmp} renderInput={(params) => <CustomTextField {...params} variant="outlined" placeholder="Select a playlist" />}  sx={{width: '50vw', backgroundColor: 'rgb(255,255,255,0.5)', borderRadius: '20px', outline: 'none'}} />
-                            <Button sx={{height: '100%'}}>CONFIRM</Button>
+                        <Stack direction="row" sx={{marginTop: '30px', display: 'flex', alignItems: 'center'}}>
+                            <Autocomplete onChange={handleInputChange} disablePortal 
+                            options={playlists} 
+                            getOptionLabel={(option) => option.name}
+                            renderInput={(params) => <CustomTextField {...params} variant="outlined" placeholder="Select a playlist" 
+                            sx={{input: {color: 'rgb(255, 255, 255, 0.9)', fontFamily: 'Montserrat, sans-serif'}}}
+                            />}  sx={{width: '50vw', backgroundColor: 'rgb(255,255,255,0.2)', borderRadius: '20px', outline: 'none'}} />
+                            <CustomButton onClick={processQuery} sx={{height: '95%', marginLeft: '1vw'}}>CONFIRM</CustomButton>
                         </Stack>
                     </Stack>
                     
