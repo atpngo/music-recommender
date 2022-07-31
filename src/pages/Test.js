b6ea677887e5280a5e9c0970a82aa5c433895e98
Andy Ngo <andytpngo@g.ucla.edu>
2022-07-30 17:48:12 -0700

    changed layout for popup

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -3,7 +3,7 @@ import { Howl } from "howler";
 import React, { useEffect, useState } from "react";
 import CurrentSong from "../components/CurrentSong";
 import Slide from '@mui/material/Slide';
-import {Dialog, Stack, Paper, List} from '@mui/material';
+import {Dialog, Stack, Paper, List, Autocomplete, TextField, Button} from '@mui/material';
 import AlbumCover from "../components/AlbumCover";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -66,14 +66,23 @@ function Test()
         title: 'Her Song',
         artist: 'Kaylee Federmann',
     }
+
+    const tmp = [
+        'Cow1',
+        'Cow1',
+        'Cow1',
+        'Cow1',
+        'Cow1',
+        'Cow1',
+    ]
     return(
         <div>
             <Dialog maxWidth='xl' open={true} TransitionComponent={Transition} keepMounted PaperProps={{style: {borderRadius: 20, backgroundColor: '#E28BBA'}}}>
                 <div style={{width: '75vw', height: '75vh', display: 'flex', padding: '30px'}}>
-                    <Stack direction="row">
+                    <Stack direction="column" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                         {/* contains songs */}
-                        <Paper elevation={10} sx={{backgroundColor: 'rgb(255,255,255,0.25)', height: '90%', width: '50vw', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', overflow: 'hidden', alignContent: 'flex-start', padding: '30px', paddingLeft: '0px', paddingRight: '0px', borderRadius: '20px'}}>
-                        <Paper elevation={0} sx={{backgroundColor: 'rgb(255,255,255,0)', height: '90%', width: '50vw', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', overflow: 'auto', alignContent: 'flex-start', justifyContent: 'center', padding: '30px', borderRadius: '20px'}}>
+                        <Paper elevation={10} sx={{backgroundColor: 'rgb(255,255,255,0.25)', height: '90%', width: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', overflow: 'hidden', alignContent: 'flex-start', padding: '30px', paddingLeft: '0px', paddingRight: '0px', borderRadius: '20px'}}>
+                        <Paper elevation={0} sx={{backgroundColor: 'rgb(255,255,255,0)', height: '90%', width: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', overflow: 'auto', alignContent: 'flex-start', justifyContent: 'center', padding: '30px', borderRadius: '20px'}}>
                             {JSON.parse(localStorage.getItem("savedSongs")).map((song) => {
                                 // return 
                                 // need to make something that looks like this:
@@ -86,6 +95,10 @@ function Test()
                         </Paper>
                         </Paper>
                         {/* contains options */}
+                        <Stack direction="row" sx={{marginTop: '30px'}}>
+                            <Autocomplete disablePortal options={tmp} renderInput={(params) => <TextField {...params} color="green" variant="outlined" label="Select Playlist"/>} sx={{width: '50vw', backgroundColor: 'rgb(255,255,255,0.5)', borderRadius: '20px', outline: 'none'}} />
+                            <Button sx={{height: '100%'}}>CONFIRM</Button>
+                        </Stack>
                     </Stack>
                     
                 </div>
