a233f501fbb00176b3639e205753c024bb523c24
Andy Ngo <andytpngo@g.ucla.edu>
2022-08-01 09:38:51 -0700

    finished creating a responsive landing page

--- src/pages/Authenticate.js
+++ src/pages/Authenticate.js
@@ -2,6 +2,42 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Credentials from "../Credentials";
 import { useNavigate } from "react-router-dom";
+import demo from '../media/demo.mp4';
+import logo from '../media/logo.png';
+import { Button, Paper, Stack } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import spotifyLogo from '../media/spotify.png';
+
+// Spotify Button
+function SpotifyButton(props)
+{
+
+    const CustomButton = styled(Button)(({theme}) => ({
+        color: 'white',
+        fontSize: '2vw',
+        lineHeight: '1em',
+        textTransform: 'none',
+        backgroundColor: '#7DE774',
+        width: '50%',
+        height: '15vh',
+        borderRadius: '30px',
+        '&:hover' : {
+            backgroundColor: '#73e069'
+        }
+    }))
+    return(
+        <CustomButton component={Paper} elevation={5} onClick={props.onClick}>
+            <Stack direction="row" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <img src={spotifyLogo} width='20%' height='20%'/>
+                
+                <Stack direction="column" sx={{marginLeft: '40px', display: 'flex', alignItems: 'center'}}>
+                    <p>Login with</p>
+                    <p>Spotify</p>
+                </Stack>
+            </Stack>
+        </CustomButton>
+    )
+}
 
 function Authenticate()
 {
@@ -41,9 +77,41 @@ function Authenticate()
         window.location = `${spotify.AUTHORIZE_ENDPOINT}?client_id=${spotify.CLIENT_ID}&redirect_uri=${spotify.REDIRECT_URL}&scope=${scopes}&response_type=token&show_dialog=true`;
     }
 
+    const debug = () =>
+    {
+
+    }
+
+    const subtitle = {
+        fontSize:'1.25vw', color: 'white'
+    }
+
     return(
-        <div>
-            <button onClick={login}>Authenticate with Spotify</button>
+        <div style={{display: 'flex', height: '100vh',alignItems: 'center', justifyContent: 'center'}}>
+            <Stack direction="column" style={{display: 'flex', alignItems: 'center'}}>
+            <Stack direction="row">
+                <Stack direction="column" style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '50%'}}>
+                    {/* <button onClick={debug}>Debug</button> */}
+                    <img width="90%" src={logo}/>
+                    <div style={{marginTop: '2vh'}}/>
+                    <p style={subtitle}>
+                        Find your favorite <strong>singles</strong> :D
+                    </p>
+                    <p style={subtitle}>hahahah lol</p>
+                    <div style={{marginTop: '10vh'}}/>
+                    <SpotifyButton onClick={login}/>
+                </Stack>
+                <div style={{marginLeft: '5%'}}></div>
+                {/* Demo video */}
+                <Paper elevation={10} sx={{width: '40vw', height: '30%',backgroundColor: 'rgb(255,255,255,0.25)', padding: '15px 15px 12px 15px', borderRadius: '10px'}}>
+                    <video width='100%' muted autoPlay={"autoplay"} preLoad="auto" loop src={demo}/>
+                </Paper>
+                
+            </Stack>
+            <p style={{marginTop: '10%'}}>
+                Made by Andy
+            </p>
+            </Stack>
         </div>
     );
 }
