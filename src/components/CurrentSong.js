c38d2bbf7815443ab0a4fdeb9ce16c1a7aa53fda
Andy Ngo <andytpngo@g.ucla.edu>
2022-08-04 20:24:18 -0700

    slightly better mobile fit

--- src/components/CurrentSong.js
+++ src/components/CurrentSong.js
@@ -12,9 +12,9 @@ function CurrentSong(props)
     
     return(
         <div>
-            <Paper elevation={10} sx={{width: (width >= height) ? '40vw' : '80vw', height: '85vh', backgroundColor: 'rgb(255,255,255,0.18)', borderRadius: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <Paper elevation={10} sx={{width: (width >= height) ? '40vw' : '90vw', height: (width >= height) ? '85vh' : (width <= 450) ? '65vh' : '85vh', backgroundColor: 'rgb(255,255,255,0.0)', borderRadius: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                 <Stack direction="column" sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                    <Avatar component={Paper} elevation={5} variant="square" src={props.song.image} sx={{width: (width > height) ? '60%' : '60%', height: "auto"}} imgProps={{draggable: false}}/>
+                    <Avatar component={Paper} elevation={5} variant="square" src={props.song.image} sx={{width: (width > height) ? '60%' : '75%', height: "auto"}} imgProps={{draggable: false}}/>
 
                     {/* Song Name */}
                     {/* <p style={{fontSize: '2.5em', marginBottom: '10px', marginTop: '10px', color: 'white'}}>{props.song.title}</p> */}
